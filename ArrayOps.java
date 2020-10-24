aafe76b7ef426af2b68dda839500febee70d2a69
ava-lu <alu21@stuy.edu>
2020-10-24 00:42:55 -0400

    Added largest() function, but still may be more cases to test

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,3 +1,4 @@
+//git add,commit,push
 public class ArrayOps {
   public static int sum(int[] arr) {
     int sum = 0;
@@ -6,6 +7,14 @@ public class ArrayOps {
       sum = sum + num;
     }
     return sum;
+  }
+
+  public static int largest(int[]arr) {
+    int temp = arr[0];
+    for (int i=0; i<arr.length; i++) {
+      if (arr[i]>temp) temp = arr[i];
+    }
+    return temp;
   }
 
 }
--- Tester.java
+++ Tester.java
@@ -1,7 +1,7 @@
 public class Tester {
   public static void main(String[] args) {
-    int[]A = {4,11,2};
-    System.out.println(ArrayOps.sum(A));
+    int[]A = {4,11,2,16,3,10,7};
+    System.out.println(ArrayOps.largest(A));
   }
 
 }
