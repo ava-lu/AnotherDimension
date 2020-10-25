af271d77e7ad7b8227334eca34e6649ecdc1e025
ava-lu <alu21@stuy.edu>
2020-10-25 00:10:45 -0400

    Finished sum() of 2d arrays and tested case

--- ArrayOps.java
+++ ArrayOps.java
@@ -35,4 +35,13 @@ public class ArrayOps {
     return arr;
   }
 
+  public static int sum(int[][] arr) {
+    int newarr[] = new int[arr.length];
+    for (int i=0; i<arr.length; i++) {
+      newarr[i] = sumRows(arr)[i];
+    }
+    return(sum(newarr));
+
+  }
+
 }
