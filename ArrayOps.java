09e4bb6ef3e44dcf20788f2a982fe4570b05b1d6
ava-lu <alu21@stuy.edu>
2020-10-24 20:55:37 -0400

    Added sumRows() method, have not tested yet

--- ArrayOps.java
+++ ArrayOps.java
@@ -17,4 +17,13 @@ public class ArrayOps {
     return temp;
   }
 
+  public static int[] sumRows(int[][] matrix) {
+    int arr[] = new int[matrix.length];
+    for (int i=0; i<arr.length; i++) {
+      int tempsum = sum(matrix[i]);
+      arr[i] = tempsum;
+    }
+    return arr;
+  }
+
 }
