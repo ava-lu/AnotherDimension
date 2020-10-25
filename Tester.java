a43bc5e71df541e4ab674ebcd71623aa2428331f
ava-lu <alu21@stuy.edu>
2020-10-25 11:21:02 -0400

    Finished isColMagic() and tested cases

--- Tester.java
+++ Tester.java
@@ -3,6 +3,9 @@ public class Tester {
   public static void main(String[] args) {
     int[][]A = { {1,0,12,-1}, {7,-2,2,1}, {-5,-2,2,-9}};
     int[]B = {1,3,5};
+    int[][]C = { {1,2,3,4}, {2,3,4,1}, {3,4,1,2}};
+    int[][]D = { {1,1,1}, {2,2,2}, {3,3,3}};
+    int[][]E = { {2,2,2}, {2,2,2}};
 
 // sum of 1D array
     System.out.println(ArrayOps.sum(B));
@@ -17,7 +20,9 @@ public class Tester {
 // sum of each column
     System.out.println(Arrays.toString(ArrayOps.sumCols(A)));
 // does each row have same sum
-    System.out.println(ArrayOps.isRowMagic(A));
+    System.out.println(ArrayOps.isRowMagic(E));
+// does each column have same sum
+    System.out.println(ArrayOps.isColMagic(E));
   }
 
 }
