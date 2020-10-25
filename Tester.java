82d3c867f5455ee208c4293912b77348e816b35f
ava-lu <alu21@stuy.edu>
2020-10-24 23:54:13 -0400

    Tested sumRows() and added my test cases for the previous methods

--- Tester.java
+++ Tester.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
 public class Tester {
   public static void main(String[] args) {
-    int[]A = {4,11,2,16,3,10,7};
-    System.out.println(ArrayOps.largest(A));
+    int[][]A = { {1,0,12,-1}, {7,-2,2,1}, {-5,-2,2,-9}};
+    int[]B = {1,3,5};
+    System.out.println(ArrayOps.sum(B));
+    System.out.println(ArrayOps.largest(B));
+    System.out.println(Arrays.toString(ArrayOps.sumRows(A)));
   }
 
 }
